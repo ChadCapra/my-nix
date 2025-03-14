d2e1af1d8c6de772494f5674262cd2742529a589
Chad Capra <chadcapra@gmail.com>
2025-03-14 14:45:49 -0500

    configure starship prompt

--- cli/init.nix
+++ cli/init.nix
@@ -24,19 +24,32 @@
 		};
 	};
 
-	programs.starship = {
-		enable = true;
-		settings = {
-			add_newline = false;
-		};
-	};
-
 	programs.nushell = {
 		enable = true;
 		configFile.text = ''
 			alias ll = ls -l
 			alias gs = git status
 		'';
+	};
+
+	programs.starship = {
+		enable = true;
+		settings = {
+			add_newline = false;
+			format = "$hostname$directory$git_branch$character";  # Custom prompt format
+				directory = {
+					truncation_length = 3;  # Only show the last 3 directories
+					style = "bold yellow";
+				};
+			git_branch = {
+				format = "[$symbol$branch]($style) ";  # Make Git branch more visible
+				symbol = "🌱 ";  # Change Git branch symbol
+			};
+			character = {
+				success_symbol = "[➜](bold green) ";  # Green arrow for success
+				error_symbol = "[✗](bold red) ";  # Red X for errors
+			};
+		};
 	};
 
 	home.packages = with pkgs; [
